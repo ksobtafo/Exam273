40223bb10dbc2ce66b78499b144e507709660b06
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2018-11-20 15:51:19 -0800

    Create dinnerEvent.h

---
+++ dinnerEvent.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include "event.h"
+#ifndef DINNEREVENT_H
+#define DINNEREVENT_H
+
+class dinnerEvent: class event
+{
+private:
+	std::list<std::string>room;
+	dinnerEvent() {};
+	friend class 
+public:
+
+};
+#endif
