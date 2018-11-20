6087b4885809a1cba86859a2e57c111892a16b1d
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2018-11-20 15:53:45 -0800

    Create PlanMtgInvite.h

---
+++ PlanMtgInvite.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+#include "event.h"
+#ifndef PLANMTGINVITE_H
+#define PLANMTGINVITE_H
+
+
+class PlanMtgInvite
+{
+private:
+	event * this_party;
+	std::list<std::string>::iterator me;
+public:
+	void leave()
+	{
+		                                                         //FIX ME 
+	}
+
+};
+#endif
